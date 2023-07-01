6c448a03f67ad7782e418f8295a8d1377c1a4f32
Gabriel-Leal <gabriel.leal.s.oliveira@gmail.com>
2023-07-01 17:04:29 -0400

    added buttons Deposit and Debited

--- project-03/src/components/NewTransactionModal/index.tsx
+++ project-03/src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,13 @@
 import * as Dialog from "@radix-ui/react-dialog";
-import { CloseButton, Content, Overlay } from "./styles";
-import { X } from "phosphor-react";
+import {
+  CloseButton,
+  Content,
+  Overlay,
+  TransactionType,
+  TransactionTypeButton,
+} from "./styles";
+import { X, ArrowCircleUp, ArrowCircleDown } from "phosphor-react";
+
 export function NewTransactionModal() {
   return (
     <Dialog.Portal>
@@ -14,6 +21,15 @@ export function NewTransactionModal() {
           <input type="text" placeholder="Description" />
           <input type="number" placeholder="$" />
           <input type="text" placeholder="Category" />
+
+          <TransactionType>
+            <TransactionTypeButton variant="income" value="income">
+              <ArrowCircleUp size={24} /> Deposit
+            </TransactionTypeButton>
+            <TransactionTypeButton variant="outcome" value="outcome">
+              <ArrowCircleDown size={24} /> Debited
+            </TransactionTypeButton>
+          </TransactionType>
           <button type="submit">Register</button>
         </form>
       </Content>
