7f0c90fb5368f515c20cefa4e9a1417810bff7db
Gabriel-Leal <gabriel.leal.s.oliveira@gmail.com>
2023-07-01 14:23:28 -0400

    added Table

--- project-03/src/pages/transations/index.tsx
+++ project-03/src/pages/transations/index.tsx
@@ -1,11 +1,54 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import {
+  PriceHighLight,
+  TransactionsTable,
+  TrasactionsContainer,
+} from "./styles";
 
 export function Transactions() {
   return (
     <>
       <Header />
       <Summary />
+      <TrasactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Developing website</td>
+              <td>
+                <PriceHighLight variant="income"> $17,000.00</PriceHighLight>
+              </td>
+              <td>Category</td>
+              <td>04/09/2023</td>
+            </tr>
+            <tr>
+              <td width="50%">Developing website</td>
+              <td>
+                <PriceHighLight variant="outcome"> $17,000.00</PriceHighLight>
+              </td>
+              <td>Category</td>
+              <td>04/09/2023</td>
+            </tr>
+            <tr>
+              <td width="50%">Developing website</td>
+              <td>
+                <PriceHighLight variant="income"> $17,000.00</PriceHighLight>
+              </td>
+              <td>Category</td>
+              <td>04/09/2023</td>
+            </tr>
+            <tr>
+              <td width="50%">Developing website</td>
+              <td>
+                <PriceHighLight variant="outcome"> $17,000.00</PriceHighLight>
+              </td>
+              <td>Category</td>
+              <td>04/09/2023</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TrasactionsContainer>
     </>
   );
 }
